313f90b4ec760a18c18caae3cdbc8b4f97494a00
Scorpio <Scorpion.TMN@ya.ru>
2026-02-21 18:51:00 +0700

    KBS-0-0-Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.3.10"
+}
+
+group = "kbs.r54-nsk.ru"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(19)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
