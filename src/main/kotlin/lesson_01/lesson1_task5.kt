c11a0fcb3795eb2730ec47ff9db90443ddd2c80e
Scorpio <Scorpion.TMN@ya.ru>
2026-02-21 23:32:29 +0700

    KBS-1-5

---
+++ src/main/kotlin/lesson_01/lesson1_task5.kt
@@ -0,0 +1,20 @@
+package lesson_01
+
+fun main(args: Array<String>) {
+    val seconds = 6480
+    val hours: Int
+    val minutes: Int
+    val remainingSeconds: Int
+    val remainingMinutes: Int
+
+    minutes = (seconds / 60).toString().padStart(2, '0').toInt()
+    hours = (minutes / 60).toString().padStart(2, '0').toInt()
+    remainingMinutes = minutes % 60
+    remainingSeconds = seconds % 60
+
+    println(
+        "${hours.toString().padStart(2, '0')}:" +
+                "${remainingMinutes.toString().padStart(2, '0')}:" +
+                "${remainingSeconds.toString().padStart(2, '0')}"
+    )
+}
