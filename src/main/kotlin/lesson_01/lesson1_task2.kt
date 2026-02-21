3e7d360b77f70012fdbde54d2783c90d11610771
Scorpio <Scorpion.TMN@ya.ru>
2026-02-21 20:10:47 +0700

    KBS-1-2-adding a variable staffCount

---
+++ src/main/kotlin/lesson_01/lesson1_task2.kt
@@ -0,0 +1,11 @@
+package lesson_01
+
+fun main() {
+    val orderCount = 75
+    val thankYouMessage = "Спасибо за Ваш заказ"
+    val staffCount = 2000
+
+    println(orderCount)
+    println(thankYouMessage)
+    println(staffCount)
+}
