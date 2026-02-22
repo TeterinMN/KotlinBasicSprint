7dcf293fc836d9ca9353ab6e9c53624c3f1f4f53
TeterinMN <Scorpio@r54-nsk.ru>
2026-02-22 20:06:54 +0700

    KS-2-4-решение задачи 4 к уроку 2

---
+++ src/main/kotlin/lesson_02/lesson2_task4.kt
@@ -0,0 +1,10 @@
+package lesson_02
+
+fun main() {
+    val crystalOre = 7
+    val ironOre = 11
+
+    println("Количество бонусов к кристаллической руде: ${(crystalOre * 20) / 100}")
+    println("Количество бонусов к железной руде: ${(ironOre * 20) / 100}")
+
+}
