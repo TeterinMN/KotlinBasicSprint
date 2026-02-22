386a69e8d615e625fa34cdeaabf80df71b6b872c
TeterinMN <Scorpio@r54-nsk.ru>
2026-02-22 19:48:35 +0700

    KS-2-3-решение задачи 3 к уроку 2

---
+++ src/main/kotlin/lesson_02/lesson2_task3.kt
@@ -0,0 +1,14 @@
+package lesson_02
+
+fun main(args: Array<String>) {
+    val departureHour = 9
+    val departureMinutes = 39
+    val trainTravelTime = 457
+    val timeMinutes = (departureHour * 60) + departureMinutes
+    val totalTimeMinutes = timeMinutes + trainTravelTime
+
+    val arrivalHour = totalTimeMinutes / 60
+    val arrivalMinute = totalTimeMinutes % 60
+
+    println(("$arrivalHour:$arrivalMinute").padStart(2, '0'))
+}
