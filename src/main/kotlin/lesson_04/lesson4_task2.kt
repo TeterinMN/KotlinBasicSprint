b1252ef6ba1e6326877c052f69cb9a87047f36e2
TeterinMN <Scorpio@r54-nsk.ru>
2026-02-24 22:35:09 +0700

    KS-4-2-решение задачи 2 к уроку 4

---
+++ src/main/kotlin/lesson_04/lesson4_task2.kt
@@ -0,0 +1,18 @@
+package lesson_04
+
+const val MIN_WEIGHT = 35
+const val MAX_WEIGHT = 100
+const val MAX_VOLUME = 100
+
+fun main() {
+    val cargoWeight = 50
+    val cargoVolume = 100
+
+    println(
+        "Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average':" +
+                "${
+                    (cargoWeight > MIN_WEIGHT) && (cargoWeight <= MAX_WEIGHT)
+                            && (cargoVolume < MAX_VOLUME)
+                }"
+    )
+}
