7550db917f2bcd9312749fb219d241f206fc6eea
TeterinMN <Scorpio@r54-nsk.ru>
2026-02-24 08:00:46 +0700

    KS-2-4-объявление переменных

--- src/main/kotlin/lesson_02/lesson2_task4.kt
+++ src/main/kotlin/lesson_02/lesson2_task4.kt
@@ -3,8 +3,10 @@ package lesson_02
 fun main() {
     val crystalOre = 7
     val ironOre = 11
+    val bonusPercent = 20
+    val fullPercent = 100
 
-    println("Количество бонусов к кристаллической руде: ${(crystalOre * 20) / 100}")
-    println("Количество бонусов к железной руде: ${(ironOre * 20) / 100}")
+    println("Количество бонусов к кристаллической руде: ${(crystalOre * bonusPercent) / fullPercent}")
+    println("Количество бонусов к железной руде: ${(ironOre * bonusPercent) / fullPercent}")
 
 }
