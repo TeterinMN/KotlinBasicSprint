cb23212134c3161c4a55f64bb4303083e0c24b5e
TeterinMN <Scorpio@r54-nsk.ru>
2026-02-24 07:53:27 +0700

    KS-2-3-объявление константы, корректировка формата вывода

--- src/main/kotlin/lesson_02/lesson2_task3.kt
+++ src/main/kotlin/lesson_02/lesson2_task3.kt
@@ -1,14 +1,14 @@
 package lesson_02
-
+const val MINUTES_IN_HOUR = 60
 fun main() {
     val departureHour = 9
     val departureMinutes = 39
     val trainTravelTime = 457
-    val timeMinutes = (departureHour * 60) + departureMinutes
+    val timeMinutes = (departureHour * MINUTES_IN_HOUR) + departureMinutes
     val totalTimeMinutes = timeMinutes + trainTravelTime
 
-    val arrivalHour = totalTimeMinutes / 60
-    val arrivalMinute = totalTimeMinutes % 60
+    val arrivalHour = totalTimeMinutes / MINUTES_IN_HOUR
+    val arrivalMinute = totalTimeMinutes % MINUTES_IN_HOUR
 
-    println(("$arrivalHour:$arrivalMinute").padStart(2, '0'))
+    println("%02d:%02d".format(arrivalHour, arrivalMinute))
 }
