7cf58ed6e8a6f577807d93f9166af43bbe7a7293
TeterinMN <Scorpio@r54-nsk.ru>
2026-02-25 22:25:33 +0700

    KS-4-5-решение задачи 5 к уроку 4

---
+++ src/main/kotlin/lesson_04/lesson4_task5.kt
@@ -0,0 +1,27 @@
+package lesson_04
+
+const val DAMAGE = false
+const val PROVISIONS = 50
+const val MIN_CREW = 55
+const val MAX_CREW = 70
+const val FAVORABLE_WEATHER = true
+fun main() {
+    print("Наличие повреждений корпуса (true/false): ")
+    val damage = readln().toBoolean()
+    print("Текущий состав экипажа: ")
+    val crew = readln().toInt()
+    print("Количество ящиков с провизией: ")
+    val provisions = readln().toInt()
+    print("Благоприятность метеоусловий (true/false): ")
+    val favorableWeather = readln().toBoolean()
+
+    val result =
+        (damage == DAMAGE && favorableWeather == FAVORABLE_WEATHER)
+                && (crew >= MIN_CREW && crew <= MAX_CREW)
+                    && (provisions > PROVISIONS)
+                || (damage != DAMAGE && favorableWeather == FAVORABLE_WEATHER)
+                    && (crew == MAX_CREW && provisions >= PROVISIONS)
+
+    println(result)
+
+}
