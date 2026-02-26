58f201fdbaa9e1d812babfe33499cbc703b1fcec
TeterinMN <Scorpio@r54-nsk.ru>
2026-02-26 22:04:52 +0700

    KS-4-3-изменено форматирование, переименовал переменные

--- src/main/kotlin/lesson_04/lesson4_task3.kt
+++ src/main/kotlin/lesson_04/lesson4_task3.kt
@@ -1,16 +1,16 @@
 package lesson_04
 
-const val SUNNY_WITHER = true
-const val AWNING_OPEN = true
+const val IS_SUNNY = true
+const val IS_AWNING_OPEN = true
 const val AIR_HUMIDITY = 20
 const val SEASON = "Зима"
 
 fun main() {
-    val todayWeather = true
-    val isAwningOpenNow = true
+    val isSunny = true
+    val isAwningOpen = true
     val awningHumidity = 20
     val seasonNow = "Зима"
-    val result =
-        (todayWeather == SUNNY_WITHER) && (isAwningOpenNow == AWNING_OPEN) && (awningHumidity == AIR_HUMIDITY) && (seasonNow != SEASON)
+    val result = (isSunny == IS_SUNNY) && (isAwningOpen == IS_AWNING_OPEN) &&
+            (awningHumidity == AIR_HUMIDITY) && (seasonNow != SEASON)
     println("Благоприятные ли условия сейчас для роста бобовых? $result")
 }
