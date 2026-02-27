7a9c681a5b9130cd2b4f699c57e039bfc8f7b03c
TeterinMN <Scorpio@r54-nsk.ru>
2026-02-27 20:18:39 +0700

    KS-5-1-убрал повторение изменил переменные

--- src/main/kotlin/lesson_05/lesson5_task1.kt
+++ src/main/kotlin/lesson_05/lesson5_task1.kt
@@ -1,12 +1,12 @@
 package lesson_05
 
 fun main() {
-	val captcha = 87 + 13
-	val captchaText = "87 + 13"
+    val captchaLeftOperand = 87
+    val captchaRightOperand = 13
 
-	print("Подтвердите что вы не бот, решите пример $captchaText = ")
-	val userAnswer = readln().toInt()
+    print("Подтвердите что вы не бот, решите пример $captchaLeftOperand + $captchaRightOperand = ")
+    val userAnswer = readln().toInt()
 
-	val result = if (userAnswer == captcha) "Добро пожаловать!" else "Доступ запрещен."
-	println(result)
+    val result = if (userAnswer == captchaLeftOperand + captchaRightOperand) "Добро пожаловать!" else "Доступ запрещен."
+    println(result)
 }
