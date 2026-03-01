8efb6ac6ec934bc69ba04a2e890ef0319254cf17
TeterinMN <Scorpio@r54-nsk.ru>
2026-03-01 18:45:15 +0700

    KS-5-3-убрал переменную и конструкцию if...else

--- src/main/kotlin/lesson_05/lesson5_task3.kt
+++ src/main/kotlin/lesson_05/lesson5_task3.kt
@@ -1,21 +1,22 @@
 package lesson_05
 
+const val MIN_VALUE = 0
+const val MAX_VALUE = 42
 fun main() {
-	val numberOne = 5
-	val numberTwo = 17
-	var userInput: String
+    val numberOne = 5
+    val numberTwo = 17
 
-	print("Введите первое целое число от 0 до 42: > ")
-	userInput = readln()
-	val userNumberOne = if (userInput.toInt() == numberOne || userInput.toInt() == numberTwo) userInput.toInt() else 0
+    print("Введите первое целое число от $MIN_VALUE до $MAX_VALUE: > ")
+    val userNumberOne = readln().toInt()
 
-	print("Введите второе целое число от 0 до 42: > ")
-	userInput = readln()
-	val userNumberTwo = if (userInput.toInt() == numberOne || userInput.toInt() == numberTwo) userInput.toInt() else 0
+    print("Введите второе целое число от $MIN_VALUE до $MAX_VALUE: > ")
+    val userNumberTwo = readln().toInt()
 
-	if ((userNumberOne == numberOne && userNumberTwo == numberTwo) || (userNumberOne == numberTwo && userNumberTwo == numberOne))
-		println("Поздравляем! Вы выиграли главный приз!")
-	else if ((userNumberOne == numberOne || userNumberTwo == numberTwo) || (userNumberOne == numberTwo || userNumberTwo == numberOne))
-		println("Вы выиграли утешительный приз!") else println("Неудача")
-	println("Выигрышные числа $numberOne и $numberTwo")
+    if ((userNumberOne == numberOne && userNumberTwo == numberTwo) || (userNumberOne == numberTwo && userNumberTwo == numberOne)) println(
+        "Поздравляем! Вы выиграли главный приз!"
+    )
+    else if ((userNumberOne == numberOne || userNumberTwo == numberTwo) || (userNumberOne == numberTwo || userNumberTwo == numberOne)) println(
+        "Вы выиграли утешительный приз!"
+    ) else println("Неудача")
+    println("Выигрышные числа $numberOne и $numberTwo")
 }
