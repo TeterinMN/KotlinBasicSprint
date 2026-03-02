446c2a6923e598cac5fab545d125b235fd3f3b02
TeterinMN <Scorpio@r54-nsk.ru>
2026-03-02 23:09:02 +0700

    KS-6-4 решение задачи 4 к уроку 6

--- src/main/kotlin/lesson_06/lesson6_task4.kt
+++ src/main/kotlin/lesson_06/lesson6_task4.kt
@@ -1,14 +1,26 @@
 package lesson_06
+
 const val MIN_NUMBER = 1
 const val MAX_NUMBER = 9
 const val NUMBER_OF_ATTEMPTS = 5
 
 fun main() {
-    val hiddenNumber = (MIN_NUMBER .. MAX_NUMBER).random()
+    val hiddenNumber = (MIN_NUMBER..MAX_NUMBER).random()
     println(hiddenNumber)
-    var userNumber = readln().toInt()
 
-    while (hiddenNumber != userNumber) {
-        if (userNumber )
+    var counter = NUMBER_OF_ATTEMPTS
+
+    while (true) {
+        counter--
+        print("Введите число от $MIN_NUMBER до $MAX_NUMBER: ")
+        val userNumber = readln().toInt()
+        if (userNumber == hiddenNumber) {
+            println("Это была великолепная игра!")
+            break
+        } else println("Вы ошиблись, осталось попыток: $counter")
+        if (counter <= 0) {
+            println("Вы исчерпали все свои попытки, было загадано число $hiddenNumber")
+            break
+        }
     }
 }
