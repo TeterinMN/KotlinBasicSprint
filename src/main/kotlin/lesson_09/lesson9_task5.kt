cb0c29675c82a5439ced57721e9a4c450663f465
TeterinMN <Scorpio@r54-nsk.ru>
2026-03-10 22:45:15 +0700

    KS-9-5-решение задачи 5 к уроку 9

---
+++ src/main/kotlin/lesson_09/lesson9_task5.kt
@@ -0,0 +1,18 @@
+package lesson_09
+
+const val LIMIT_INGREDIENTS = 5
+
+fun main() {
+    val ingredients = mutableListOf<String>()
+    println("Введите 5 названий ингредиентов: ")
+    var counter = 0
+    while (ingredients.size < LIMIT_INGREDIENTS) {
+        counter++
+        print("$counter. ")
+        ingredients.add(readln())
+    }
+    val sortIngredients = ingredients.sorted().toMutableList()
+    sortIngredients[0] = sortIngredients[0].replaceFirstChar { it.uppercase() }
+
+    println(sortIngredients.joinToString(", "))
+}
