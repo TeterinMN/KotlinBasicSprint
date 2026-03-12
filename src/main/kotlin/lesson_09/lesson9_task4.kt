888365024455709319354b0902d8bc132b79bfa5
TeterinMN <Scorpio@r54-nsk.ru>
2026-03-12 20:25:30 +0700

    KS-9-4-вынес в константу кол-во ингредиентов

--- src/main/kotlin/lesson_09/lesson9_task4.kt
+++ src/main/kotlin/lesson_09/lesson9_task4.kt
@@ -1,12 +1,14 @@
 package lesson_09
 
+const val INGREDIENTS_COUNT = 5
+
 fun main() {
-    print("Введите 5 названий ингредиентов, через запятую с пробелом: ")
+    print("Введите $INGREDIENTS_COUNT названий ингредиентов, через запятую с пробелом: ")
     val inputUser = readln()
 
     val ingredients = inputUser.split(", ").toMutableList()
     val sortIngredients = ingredients.sorted()
-    if (ingredients.size == 5) println(sortIngredients)
-    else println("Вы ввели количество ингредиентов не равное 5")
+    if (ingredients.size == INGREDIENTS_COUNT) println(sortIngredients)
+    else println("Вы ввели количество ингредиентов не равное $INGREDIENTS_COUNT")
 
 }
