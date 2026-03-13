8d75c717a36fbf675b693764a1addc0d937a7b72
TeterinMN <Scorpio@r54-nsk.ru>
2026-03-13 21:48:04 +0700

    KS-9-5-изменил тип коллекции на mutableSetOf() добавил проверку на совпадение

--- src/main/kotlin/lesson_09/lesson9_task5.kt
+++ src/main/kotlin/lesson_09/lesson9_task5.kt
@@ -3,13 +3,15 @@ package lesson_09
 const val LIMIT_INGREDIENTS = 5
 
 fun main() {
-    val ingredients = mutableListOf<String>()
+    val ingredients = mutableSetOf<String>()
     println("Введите $LIMIT_INGREDIENTS названий ингредиентов: ")
-    var counter = 0
+    var counter = 1
     while (ingredients.size < LIMIT_INGREDIENTS) {
-        counter++
         print("$counter. ")
-        ingredients.add(readln())
+        val userInput = (readln().lowercase())
+        if (ingredients.add(userInput)) {
+            counter++
+        } else println("Такой ингредиент уже есть")
     }
     val sortIngredients = ingredients.sorted().toMutableList()
     sortIngredients[0] = sortIngredients[0].replaceFirstChar { it.uppercase() }
