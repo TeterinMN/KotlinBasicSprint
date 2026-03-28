787355bff752f256554128c129e76cc7c610effd
TeterinMN <Scorpio@r54-nsk.ru>
2026-03-28 19:55:24 +0700

    KS-10-3-внес исправления согласно замечаний

--- src/main/kotlin/lesson_10/lesson10_task3.kt
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -12,11 +12,10 @@ fun generatePassword(length: Int): String {
     val symbol = ' '..'/'
     val number = 0..9
     var password = ""
-    for (i in 1..length step 2) {
-        password += "${number.random()}${symbol.random()}"
-    }
-    if (length % 2 != 0) {
-        password = password.dropLast(1)
+    for (i in 1..length) {
+        password += if (i % 2 != 0) {
+            number.random()
+        } else symbol.random()
     }
     return password
 }
