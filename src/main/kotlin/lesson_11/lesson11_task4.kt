6846b6781cd0eed0c1b957d3182ed0e2556e9de1
TeterinMN <Scorpio@r54-nsk.ru>
2026-04-04 18:04:30 +0700

    KS-11-4-решение задачи 4 к уроку 11

---
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -0,0 +1,29 @@
+package lesson_11
+
+fun main() {
+
+}
+
+class RecipeCategory(
+    val id : Int,
+    val categoryName : String,
+    val imageUrl: String,
+)
+
+class Ingredient(
+    val id: Int,
+    val ingredientName: String,
+    val quantity: Double,
+    val unit: String,
+)
+
+class Recipe(
+    val id: Int,
+    val categoryId: Int,
+    val title: String,
+    val imageUrl: String,
+    val ingredients: List<Ingredient>,
+    val description: String,
+    val numberServings: Int,
+    val isFavorite : Boolean = false,
+)
