9f704baab262373a191faea657ca290d43deb2ee
TeterinMN <Scorpio@r54-nsk.ru>
2026-04-21 19:18:52 +0700

    KS-12-5-исправил согласно замечаний

--- src/main/kotlin/lesson_12/lesson12_task5.kt
+++ src/main/kotlin/lesson_12/lesson12_task5.kt
@@ -5,20 +5,20 @@ import kotlin.random.Random
 const val PERIOD = 30
 
 fun main() {
-    val listWeather = List(PERIOD){
-            WeatherReport(
-                dayTemperature = (0..35).random(),
-                nightTemperature = (-10..20).random(),
-                isPrecipitation = Random.nextBoolean(),
-            )
+    val listWeather = List(PERIOD) {
+        WeatherReport(
+            dayTemperature = (0..35).random(),
+            nightTemperature = (-10..20).random(),
+            isPrecipitation = Random.nextBoolean(),
+        )
     }
     val dayTemperature = listWeather.map { it.dayTemperature }.average().toInt()
     val nightTemperature = listWeather.map { it.nightTemperature }.average().toInt()
-    val isPrecipitation = listWeather.count { it.isPrecipitation }
+    val daysWithPrecipitation = listWeather.count { it.isPrecipitation }
 
     println("Средняя дневная температура за $PERIOD дней: $dayTemperature °C")
     println("Средняя ночная температура за $PERIOD дней: $nightTemperature °C")
-    println("Количество дней с осадками: $isPrecipitation")
+    println("Количество дней с осадками: $daysWithPrecipitation")
 }
 
 class WeatherReport(
