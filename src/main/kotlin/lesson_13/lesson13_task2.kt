f1f4bb1de2e3b953a7d1f983dbb3e65e41ea6ccf
TeterinMN <Scorpio@r54-nsk.ru>
2026-04-23 21:41:11 +0700

    KS-13-3 и KS-13-4 переименование класса

--- src/main/kotlin/lesson_13/lesson13_task2.kt
+++ src/main/kotlin/lesson_13/lesson13_task2.kt
@@ -1,14 +1,14 @@
 package lesson_13
 
 fun main() {
-    val addUser1 = ContactInfo("Юлия", 89963153333)
-    val addUser2 = ContactInfo("Михаил", 89963153333, "Rubi-com")
+    val addUser1 = ContactInfo_task2("Юлия", 89963153333)
+    val addUser2 = ContactInfo_task2("Михаил", 89963153333, "Rubi-com")
 
     addUser1.printContact()
     addUser2.printContact()
 }
 
-class ContactInfo(
+class ContactInfo_task2(
     val name: String,
     val phone: Long,
     val company: String? = null,
--- src/main/kotlin/lesson_13/lesson13_task3.kt
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -2,18 +2,18 @@ package lesson_13
 
 fun main() {
     val contacts = listOf(
-        ContactInfo("Михаил", 89963153333, "Rubi-com"),
-        ContactInfo("Юлия", 89147777777),
-        ContactInfo("Елизавета", 89249248888),
-        ContactInfo("Светлана", 89069109999, "Аксиома"),
-        ContactInfo("Тимур", 89139137333, "null"),
+        ContactInfo_task3("Михаил", 89963153333, "Rubi-com"),
+        ContactInfo_task3("Юлия", 89147777777),
+        ContactInfo_task3("Елизавета", 89249248888),
+        ContactInfo_task3("Светлана", 89069109999, "Аксиома"),
+        ContactInfo_task3("Тимур", 89139137333, "null"),
     )
 
     val phoneBookCompanies = contacts.mapNotNull { it.company }
     println("СПИСОК КОМПАНИЙ:\n${phoneBookCompanies.joinToString(",\n")}")
 }
 
-class ContactInfo(
+class ContactInfo_task3(
     val name: String,
     val phone: Long,
     val company: String? = null,
